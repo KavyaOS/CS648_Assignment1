69b9dce47c9ca4e836c2c6da7100fc9a3461584f
KavyaOS <kavya.os15@gmail.com>
2021-01-31 17:51:51 -0800

    Assignment1

--- public/App.js
+++ public/App.js
@@ -1,16 +1,15 @@
 "use strict";
 
-//import image from './Kavya.jpeg'
 var element = /*#__PURE__*/React.createElement("div", {
   title: "Kavya's info",
-  className: "container"
+  className: "frame"
 }, /*#__PURE__*/React.createElement("div", {
   className: "content"
 }, /*#__PURE__*/React.createElement("h1", null, " Kavya Onkarappa Sunanda "), /*#__PURE__*/React.createElement("img", {
   className: "picture",
   src: "https://avatars.githubusercontent.com/u/46484709?s=460&u=3d892da648554dbb2525be916f783d0f8eb577f3&v=4",
   alt: "Kavya"
-}), /*#__PURE__*/React.createElement("p", null, "Hi Everyone, I'm Kavya Onkarappa Sunanda.", /*#__PURE__*/React.createElement("br", null), "I'm currently pursuing Master's from San Diego State University."), /*#__PURE__*/React.createElement("a", {
+}), /*#__PURE__*/React.createElement("p", null, "Hello there, Welcome to my portal :) .", /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), "This is Kavya Onkarappa Sunanda.", /*#__PURE__*/React.createElement("br", null), "I'm currently pursuing Master's in Computer Science from San Diego State University."), /*#__PURE__*/React.createElement("a", {
   id: "button-1",
   className: "button",
   href: "https://github.com/KavyaOS",
